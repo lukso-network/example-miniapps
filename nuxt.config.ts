8f80fb3ba5e67c24124b3d99c5a27413b536ce81
Dominik Zborowski <dominik.zborowski@gmail.com>
2025-01-10 12:19:35 +0100

    Turn off ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // @ts-ignore
-import { assets } from "@lukso/web-components/tools/assets";
-import { copyAssets } from "@lukso/web-components/tools/copy-assets";
+import { assets } from '@lukso/web-components/tools/assets'
+import { copyAssets } from '@lukso/web-components/tools/copy-assets'
 
-copyAssets("./public", assets);
+copyAssets('./public', assets)
 
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ["~/assets/styles/main.scss"],
-  modules: [
-    'nuxt-graphql-client'
-  ],
+  css: ['~/assets/styles/main.scss'],
+  modules: ['nuxt-graphql-client'],
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -22,21 +20,24 @@ export default defineNuxtConfig({
     compilerOptions: {
       sourceMap: true,
       isCustomElement: (tag: string) => {
-        return tag.startsWith("lukso-");
+        return tag.startsWith('lukso-')
       },
     },
   },
-  ...({'graphql-client': {
-    watch: true,
-    autoImport: true,
-    functionPrefix: 'Gql',
-    documentPaths: ['./'],
-    preferGETQueries: false,
-    codegen: true,
-    clients: {
-      default: {
-        host: 'https://envio.lukso-mainnet.universal.tech/v1/graphql',
+  ssr: false,
+  ...({
+    'graphql-client': {
+      watch: true,
+      autoImport: true,
+      functionPrefix: 'Gql',
+      documentPaths: ['./'],
+      preferGETQueries: false,
+      codegen: true,
+      clients: {
+        default: {
+          host: 'https://envio.lukso-mainnet.universal.tech/v1/graphql',
+        },
       },
     },
-  }} as any),
-});
+  } as any),
+})
