daf3c02a158cad0f6ac3f506ee4e935139e290e0
Dominik Zborowski <dominik.zborowski@gmail.com>
2025-01-10 09:48:28 +0100

    Add web components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
+import { assets } from "@lukso/web-components/tools/assets";
+import { copyAssets } from "@lukso/web-components/tools/copy-assets";
+
+copyAssets("./public", assets);
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
+  css: ["~/assets/styles/main.scss"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  vue: {
+    compilerOptions: {
+      sourceMap: true,
+      isCustomElement: (tag: string) => {
+        return tag.startsWith("lukso-");
+      },
     },
   },
 });
